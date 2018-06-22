21c8891c0813baeafc539680d847aa681e3aaa67
Catrina <skaya.online@gmail.com>
2018-06-22 15:28:24 +0300

    hw1, part2 fixed

--- lections/lection_01_ES5-6/homework/hw_1/script.js
+++ lections/lection_01_ES5-6/homework/hw_1/script.js
@@ -17,9 +17,16 @@ let arr = [{
 }];
 
 
-let res = arr
-            .sort((a, b) => a.value - b.value)
-            .map(e => e.value);
+let res = [];
+arr.sort((a, b) => a.value - b.value)
+    .forEach(e => {
+      let num = e.value;
+      if (num <= 0) return;
+      for(let i = 2; i < num; i++) {
+        if (num % i == 0) return;
+      }
+      res.push(num);
+    });
 
 console.log('Массив отсортированный по возрастанию (по ключу "value"):', arr);
 console.log('Массив простых значений (по ключу "value"):', res);
