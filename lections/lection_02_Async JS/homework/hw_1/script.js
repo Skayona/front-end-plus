bdcf34c9a2de037d892f15d5033cab45da3c5786
Skayona <skaya.online@gmail.com>
2018-07-07 21:56:42 +0300

    1st hw optimized

--- lections/lection_02_Async JS/homework/hw_1/script.js
+++ lections/lection_02_Async JS/homework/hw_1/script.js
@@ -1,27 +1,21 @@
-function runAsyncAll(list, async) {
+async function runAsyncAll(list, async) {
   if (async) {
-    Promise
+    return Promise
       .all(list.map(item => item()))
       .then(result => console.log('async', result))
-    return;
   }
 
-  Promise
-    .resolve(list)
-    .then(async (list) => {
-      let result = [];
-      for (let item of list) {
-        await item().then(res => result.push(res));
-      }
-      console.log('sync', result)
-    })
+  let result = [];
+  for (let item of list) {
+    result.push(await item());
+  }
+  console.log('sync', result)
 }
 
 
 const f1 = () => {
   return new Promise((resolve) => {
     setTimeout(() => {
-      // console.log(`f1: 1200`);
       resolve(`f1: 1200`);
     }, 1200);
   })
@@ -30,7 +24,6 @@ const f1 = () => {
 const f2 = () => {
   return new Promise((resolve) => {
     setTimeout(() => {
-      // console.log(`f2: 300`);
       resolve(`f2: 300`);
     }, 300);
   })
@@ -39,7 +32,6 @@ const f2 = () => {
 const f3 = () => {
   return new Promise((resolve) => {
     setTimeout(() => {
-      // console.log(`f3: 1300`);
       resolve(`f3: 1300`);
     }, 1300);
   })
@@ -48,7 +40,6 @@ const f3 = () => {
 const f4 = () => {
   return new Promise((resolve) => {
     setTimeout(() => {
-      // console.log(`f4: 600`);
       resolve(`f4: 600`);
     }, 600);
   })
@@ -57,7 +48,6 @@ const f4 = () => {
 const f5 = () => {
   return new Promise((resolve) => {
     setTimeout(() => {
-      // console.log(`f5: 2500`);
       resolve(`f5: 2500`);
     }, 2500);
   })
