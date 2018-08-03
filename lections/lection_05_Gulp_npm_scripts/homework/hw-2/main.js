92abd3838931da0a14fe87fe50353e681ac32a47
Skayona <skaya.online@gmail.com>
2018-08-03 23:14:08 +0300

    img rename done

---
+++ lections/lection_05_Gulp_npm_scripts/homework/hw-2/main.js
@@ -0,0 +1,42 @@
+let fs = require('fs');
+let path = require('path');
+let initilaDir = './img';
+let finalDir = './new-img';
+let name = 'image_FR_';
+
+function copyImg(path, dest) {
+  fs.copyFile(path, dest, (err) => {
+    if (err) throw err;
+  })
+}
+
+function clearDir(path) {
+  fs.readdir(path, (err, files) => {
+    if (err) throw err;
+    if (!files.length) return;
+    for (let item of files) {
+      fs.unlinkSync(`${path}/${item}`);
+    }
+  })
+}
+
+Promise
+  .resolve(fs.existsSync(finalDir))
+  .then(res => {
+    if (res) {
+      clearDir(finalDir);
+    } else {
+      fs.mkdir(finalDir);
+    }
+  })
+  .then(() => {
+    fs.readdir(initilaDir, (err, items) => {
+      if (err) throw err;
+      items.forEach((img, i) => {
+        console.log('copyImg');
+        let ext = path.extname(img);
+        let count = (i < 9) ? `0${i+1}` : i + 1;
+        copyImg(`${initilaDir}/${img}`, `${finalDir}/${name}${count}${ext}`);
+      });
+    });
+  })
