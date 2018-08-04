3d5d1d5baac32f154b5140ef3c48eecb70bd852b
Skayona <skaya.online@gmail.com>
2018-08-04 16:10:19 +0300

    translates/gulp finished

--- lections/lection_05_Gulp_npm_scripts/homework/hw-3/gulpfile.js
+++ lections/lection_05_Gulp_npm_scripts/homework/hw-3/gulpfile.js
@@ -1,37 +1,19 @@
-var gulp = require('gulp');
-let fs = require('fs');
-let path = './translates';
-let re = /.+\./;
-let all_tr = {};
-
-gulp.task('translates', () => {
-  return gulp.src('translates')
-    .pipe(
-      fs.readdir(path, (err, items) => {
-        if (err) {
-          console.log(err);
-          return err;
-        }
-
-        Promise
-          .all(
-            items.map(item => new Promise(resolve => {
-              let lang = item.match(re)[0].slice(0, -1);
-              let translates = require(`${path}/${item}`);
-              resolve(new Map().set(lang, translates));
-            }))
-          )
-          .then((result) => {
-            result.forEach(tr => {
-              for (let key of tr.keys()) {
-                all_tr[key] = tr.get(key);
-              }
-            })
-            console.log(all_tr);
-          })
-      })
-
-    )
+const gulp = require('gulp');
+const merge = require('gulp-merge-json');
+const re = /.+\./;
 
 
+gulp.task('lang', () => {
+
+  return gulp.src('./translates/*.json')
+    .pipe(merge({
+      fileName: 'translates.json',
+      edit: (parsedJson, file) => {
+        let lang = file.relative.match(re)[0].slice(0, -1);
+        let tr = {};
+        tr[lang] = parsedJson;
+        return tr;
+      }
+    }))
+    .pipe(gulp.dest('./'));
 })
--- lections/lection_05_Gulp_npm_scripts/homework/hw-4/gulpfile.js
+++ lections/lection_05_Gulp_npm_scripts/homework/hw-4/gulpfile.js
@@ -1,6 +1,7 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass');
 
+
 gulp.task('sass', function () {
   return gulp.src(['./sass/**/*.sass', './sass/**/*.scss'])
     .pipe(sass())
