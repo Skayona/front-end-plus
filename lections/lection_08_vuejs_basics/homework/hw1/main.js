cb4725c5f5d0d3d44fedc232e999cd73c5ad1d60
Catrina <skaya.online@gmail.com>
2018-09-07 17:00:49 +0300

    wip

--- lections/lection_08_vuejs_basics/homework/hw1/main.js
+++ lections/lection_08_vuejs_basics/homework/hw1/main.js
@@ -1,20 +1,74 @@
 Vue.component(
+  'edit-modal', {
+    props: ['member', 'modal'],
+    template: `
+      <div class="modal" v-if="modal">
+        <div class="modal__content">
+          <h2>{{title}} данные</h2>
+          <label>
+            <span>ID:</span>
+            <input v-model="member.id" type="text" />
+          </label>
+          <label>
+            <span>Name:</span>
+            <input v-model="member.name" type="text" />
+          </label>
+          <label>
+            <span>Age:</span>
+            <input v-model="member.age" type="number" />
+          </label>
+          <label>
+            <span>Address:</span>
+            <textarea v-model="member.address">
+            </textarea>
+          </label>
+          <div class="modal__btn">
+            <button type="button">cancel</button>
+            <button type="button">ok</button>
+          </div>
+        </div>
+      </div>
+    `,
+    data: function () {
+      return {
+        title: this.getTitle()
+      }
+    },
+    methods: {
+      getTitle() {
+        for (let prop in this.member) {
+          if (!this.member[prop].toString().length) {
+            return 'Редактировать';
+          }
+        }
+        return 'Добавить';
+      }
+    }
+  }
+)
+
+Vue.component(
   'co-worker', {
     props: ['index'],
-    template: `<div class="co-worker" v-if="toShow" :id="index" @click="addName()">
+    template: `<div class="co-worker" :id="index" @click="$emit('open-editor', $event, addData)">
       <div class="co-worker__col co-worker__col--id"> {{ id }} </div>
       <div class="co-worker__col co-worker__col--name"> {{ name }} </div>
       <div class="co-worker__col co-worker__col--age"> {{ age }} </div>
       <div class="co-worker__col co-worker__col--address"> {{ address }} </div>
       <div class="co-worker__col co-worker__col--del">
-        <button @click="$emit('delete-member', deleteOPtions, index)">delete</button>
+        <button type="button" @click="$emit('delete-member', deleteOptions, index)">delete</button>
       </div>
     </div>`,
     methods: {
-      addName() {
-        console.log('addName');
+      addData() {
+        return {
+          id: this.id,
+          name: this.name,
+          age: this.age,
+          address: this.address
+        }
       },
-      deleteOPtions() {
+      deleteOptions() {
         if (this.id.length ||
           this.name.length ||
           this.age.length ||
@@ -23,7 +77,7 @@ Vue.component(
         }
 
         return true;
-      }
+      },
     },
     data: function () {
       return {
@@ -38,15 +92,34 @@ Vue.component(
 const vm = new Vue({
   el: '#table',
   data: {
-    members: []
+    members: [],
+    value: {},
+    openModal: false
   },
   methods: {
     addCoworker() {
       this.members.push('co-worker');
     },
-    deletes(a, i) {
-      if(a()) {
+    deleteMember(allow, i) {
+      if (allow()) {
         this.members.splice(i, 1);
+      }
+    },
+    openEditModal(e, data) {
+      if (e.target.tagName === 'BUTTON') {
+        return;
+      }
+      let member = data();
+      if (!member) return;
+      this.value = member;
+      this.openModal = true;
+
+    },
+    fixedBody() {
+      if (this.openModal) {
+        console.log('+');
+
+        return 'modal-is-open';
       }
     }
   }
